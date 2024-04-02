c37085408e92cbc9e329d0ad3b487d2fdf9cd1c2
Maruf Siyam <marufsiyam9123@gmail.com>
2024-04-02 21:39:57 +0600

    some addition

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -7,6 +7,12 @@ import { AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
   const [isClicked, setIsClicked] = useState(false);
 
+
+
+  //Temporary
+  const session = true;
+  const admin = true;
+
   return (
     <nav>
       <div className="flex w-full justify-between items-center h-[5rem] px-8 bg-slate-50 text-slate-800 shadow-xl">
@@ -14,9 +20,9 @@ const Navbar = () => {
           <h2 className="text-2xl">Logo</h2>
         </div>
         <div>
-          <ul className="md:flex gap-2 hidden">
+          <ul className="md:flex gap-2 hidden items-center">
             <Link
-              className="text-xl py-2 px-4 hover:bg-slate-400 rounded-lg duration-300 hover:text-slate-50"
+              className="text-xl py-2 px-4 hover:bg-slate-400 rounded-lg duration-300 hover:text-slate-50 "
               href={"/"}
             >
               Home
@@ -39,6 +45,32 @@ const Navbar = () => {
             >
               Blog
             </Link>
+
+            
+            
+            {
+              session ?
+              (<>
+              {
+                admin &&
+                <Link className="text-xl py-2 px-4 hover:bg-slate-400 rounded-lg duration-300 hover:text-slate-50" href={'/admin'}>Admin</Link>
+              }
+              <Link
+              className="text-xl py-2 px-4 hover:bg-slate-400 rounded-lg duration-300 rounded-sm text-slate-50 bg-slate-600"
+              href={"/logout"}
+            >
+              Logout
+            </Link>
+              </>)
+              :
+              (
+                <Link
+                className="text-xl py-2 px-4 hover:bg-slate-400 hover:rounded-lg duration-300 rounded-sm text-slate-50 bg-slate-600"
+                href={'/login'}
+                >Login</Link>
+                
+              )
+            }
           </ul>
           <div className="md:hidden ">
             <button
@@ -57,29 +89,53 @@ const Navbar = () => {
           </div>
           <ul className=" flex flex-col ">
             <Link
-              className="text-xl py-2 px-4 border-b hover:text-slate-50 duration-500"
+              className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 hover:text-slate-50 border-b border-t"
               href={"/"}
             >
               Home
             </Link>
             <Link
-              className="text-xl py-2 px-4 border-b hover:text-slate-50 duration-500"
+              className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 hover:text-slate-50 border-b"
               href={"/about"}
             >
               About
             </Link>
             <Link
-              className="text-xl py-2 px-4 border-b hover:text-slate-50 duration-500"
+              className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 hover:text-slate-50 border-b"
               href={"/contact"}
             >
               Contact
             </Link>
             <Link
-              className="text-xl py-2 px-4 border-b hover:text-slate-50 duration-500"
+              className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 hover:text-slate-50 border-b"
               href={"/blog"}
             >
               Blog
             </Link>
+
+            {
+              session ?
+              (<>
+              {
+                admin &&
+                <Link className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 hover:text-slate-50 border-b" href={'/admin'}>Admin</Link>
+              }
+              <Link
+              className="text-xl py-2 px-4 hover:bg-slate-400 duration-300 rounded-sm text-slate-50 bg-slate-600 border-b"
+              href={"/logout"}
+            >
+              Logout
+            </Link>
+              </>)
+              :
+              (
+                <Link
+                className="text-xl py-2 px-4 hover:bg-slate-400 hover:rounded-lg duration-300 rounded-sm text-slate-50 bg-slate-600 border-b"
+                href={'/login'}
+                >Login</Link>
+                
+              )
+            }
           </ul>
           <div className="px-4">
             <h2>Contact info</h2>
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const AboutPage = () => {
   return (
     <div>
         <h1>About page</h1>
+        <Image src="/about.png" alt="image" width={500} height={500}/>
     </div>
   )
 }
